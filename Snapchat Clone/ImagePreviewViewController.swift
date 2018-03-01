b13f30956f6c8165289d4c8918cedee0464c7925
dannyshiferaw <daniel.shiferaw@berkeley.edu>
2018-02-28 22:44:00 -0800

    Lab3

---
+++ Snapchat Clone/ImagePreviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImagePreviewViewController.swift
+//  Snapchat Clone
+//
+//  Created by Paige Plander on 9/27/17.
+//  Copyright © 2017 org.iosdecal. All rights reserved.
+//
+
+import UIKit
+
+/// View Controller to preview selected images. You'll need
+/// to make changes to this file
+class ImagePreviewViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var selectedImage:UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let validImage = selectedImage {
+            imageView.image = validImage
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
